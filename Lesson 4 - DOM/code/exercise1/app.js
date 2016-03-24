63536f2d8e347d1af83314fd786fcc87710284a3
michalek94 <pankos30@gmail.com>
2016-03-24 01:10:47 +0100

    validation

--- Lesson 4 - DOM/code/exercise1/app.js
+++ Lesson 4 - DOM/code/exercise1/app.js
@@ -1,4 +1,23 @@
 window.addEventListener('DOMContentLoaded', function () {
     //When the page structure is loaded...
-
+    var submitButton = document.getElementById("submit");
+    var oddNumber = document.getElementById("oddnumber");
+    var evenNumber = document.getElementById("evennumber");    
+    
+    submitButton.addEventListener("click", function () {
+        validate(oddNumber, 1);
+        validate(evenNumber, 0);    
+    }, false);
 });
+
+function validate (editText, mod) {
+    var value = +editText.value;  
+    
+    if(value%2 == mod){
+        editText.classList.remove("invalid");
+        editText.classList.add("valid");
+    }else{
+        editText.classList.remove("valid");
+        editText.classList.add("invalid");
+    }  
+};
