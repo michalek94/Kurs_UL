ba496d8a9e7fb37c34f6ac15b4af18135a616a2a
michalek94 <pankos30@gmail.com>
2016-03-24 23:47:15 +0100

    server.js

--- Lesson 6 - Developer tools/code/server.js
+++ Lesson 6 - Developer tools/code/server.js
@@ -21,12 +21,27 @@ app.get('/today', function (req, res) {
 });
 
 //POST /numbers should save number for later
-
+app.post('/numbers', function (req, res) {
+    numbers.push(req.body);
+    res.send("Number saved for later");           
+});
 //GET /numbers should return comma separated list of numbers
+app.get('/numbers', function (req, res) {
+    setTimeout(function () {
+        res.send(numbers.join(", "));
+    }, 1000);
+});
 //DELETE /numbers should empty numbers array
+app.delete('/numbers', function (req, res) {
+    numbers.length = 0;
+    res.send("Number's array is empty");
+});
 //GET /numbers/:op should return result of given operation of stored numbers (check request.params)
 // available operations are sum, difference, multiplication, division
 // return result as strings
+app.get('/numbers/:op', function (req, res) {
+    
+});
 
 app.listen(8080, function () {
     console.log('Server is running on localhost:' + this.address().port);
