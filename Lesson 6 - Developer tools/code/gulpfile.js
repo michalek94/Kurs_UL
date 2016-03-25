acb94058d5a30b22792a374f9af3af766172dade
michalek94 <pankos30@gmail.com>
2016-03-25 23:56:21 +0100

    Task 2 - Completed

---
+++ Lesson 6 - Developer tools/code/gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var gulp_concat = require('gulp-concat');
+var gulp_jshint = require('gulp-jshint');
+
+var path = {
+    scripts: ['src/*.js']  
+};
+
+gulp.task('jshint', function () {
+    gulp.src(path.scripts)
+         .pipe(gulp_jshint('.jshintrc'))
+         .pipe(gulp_jshint.reporter('default'));
+});
+
+gulp.task('concat', function () {
+    gulp.src(path.scripts)
+        .pipe(gulp_concat('calc.js'))
+        .pipe(gulp.dest('./build'));         
+});
+
+gulp.task('default', ['concat']);
